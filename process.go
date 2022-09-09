2d0a8ead622ab4135dcead2f832673eecd80ec1b
Jason McCallister <themccallister@gmail.com>
2022-09-08 21:51:18 -0400

    simple docs
    Signed-off-by: Jason McCallister <themccallister@gmail.com>

--- process.go
+++ process.go
@@ -6,13 +6,16 @@ import (
 	"syscall"
 )
 
+// Options for starting a process.
 type Options struct {
-	Name      string
-	Args      []string
-	Writer    io.Writer
-	ErrWriter io.Writer
+	Name              string
+	Args              []string
+	Writer            io.Writer
+	ErrWriter         io.Writer
+	SetProcessGroupID bool
 }
 
+// Start a process.
 func Start(o Options) error {
 	p, err := exec.LookPath(o.Name)
 	if err != nil {
@@ -20,7 +23,7 @@ func Start(o Options) error {
 	}
 
 	c := exec.Command(p, o.Args...)
-	c.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	c.SysProcAttr = &syscall.SysProcAttr{Setpgid: o.SetProcessGroupID}
 
 	c.Stdout = o.Writer
 	c.Stderr = o.ErrWriter
